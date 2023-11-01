0e4350d2fd4a2a145ec9ef691f4babed79f28f94
edjhernandez <edjhernandez@gmail.com>
2023-10-31 22:14:15 -0300

    Add basic Card structure

--- src/components/Rockets.tsx
+++ src/components/Rockets.tsx
@@ -1,6 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+
+
+
 function Rockets() {
   return (
-    <div>Hello from Rockets</div>
+    <div className="d-flex flex-column align-items-center">   
+      <Card style={{ width: '18rem' }} className='my-2'>
+        <Card.Img variant="top" src='' />
+        <Card.Body className="d-flex flex-column align-items-center">
+          <Card.Title>
+            Rocket name
+          </Card.Title>
+          <Card.Text className='text-center'>
+            Rocket Description
+          </Card.Text>
+          <Button 
+            variant = 'primary'
+          >
+            Reserve Rocket
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
