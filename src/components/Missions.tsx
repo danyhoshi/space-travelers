51bdb5accfa5dd056293ffa84be2e5e7e8121c52
dgonzalez777 <1989.daniela@gmail.com>
2023-10-31 21:26:49 -0400

    Add basic structure for missions component

--- src/components/Missions.tsx
+++ src/components/Missions.tsx
@@ -1,8 +1,38 @@
+import { Container } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table'
 
-function Missions() {
-  return (
-    <div>Missions</div>
-  )
+const Missions = () => {
+    return (
+        <Container>
+            <Table striped>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Launchpad</th>
+          <th>successes / attempts</th>
+          <th>Status</th>
+          <th>Join</th>
+        </tr>
+      </thead>
+      <tbody>
+            <tr>
+              <td>mission name</td>
+              <td><img /></td>
+              <td>mission successes / mission attempts</td>
+              <td> NOT A MEMBER</td>
+              <td>
+                <button
+                  id = '1'
+                  className='bg-info'>
+                    Join Mission
+                </button>
+              </td>
+            </tr>
+        
+      </tbody>
+    </Table>
+        </Container>
+        
+    )
 }
-
-export default Missions
+export default Missions;
