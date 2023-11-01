7700bdf6ab659fac818a76923ab2174b9b4fe8a0
edjhernandez <edjhernandez@gmail.com>
2023-10-31 21:12:09 -0300

    Add structure for MyProfile component

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -1,6 +1,19 @@
+import { Container } from "react-bootstrap"
+
+
 function MyProfile() {
   return (
-    <div>Hello from MyProfile</div>
+    <Container className="d-flex flex-row">
+      <div className="d-flex flex-column container-sm" >
+        <h2>My Missions</h2>
+      </div>
+      <div className="d-flex flex-column container-sm" >
+        <h2>My Rockets</h2>
+      </div>
+      <div className="d-flex flex-column container-sm" >
+        <h2>My Dragons</h2>
+      </div>
+    </Container>
   )
 }
 
