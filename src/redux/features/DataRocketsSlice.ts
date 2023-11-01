2a1e0269eaab3f01503329d49963168a25355304
edjhernandez <edjhernandez@gmail.com>
2023-11-01 18:49:25 -0300

    Add slice to fetch rockets data

---
+++ src/redux/features/DataRocketsSlice.ts
@@ -0,0 +1,74 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export const getDataRockets = createAsyncThunk(
+  'getDataRockets',
+  async (_, {rejectWithValue}) => {
+    const response = await fetch('https://api.spacexdata.com/v4/rockets')
+    if(!response.ok){
+        return rejectWithValue(response.status)
+    } else {
+        const data = await response.json()
+        return data
+    } 
+  }
+)
+
+export interface getData {
+    loading: boolean
+    error: string | null
+    data: data[]
+}
+
+export interface data {
+    name: string
+    image: string
+    description: string
+    id: string
+    selected?: boolean
+}
+
+const initialState = {
+    loading: false,
+    error: null,
+    data: []
+} as getData
+
+
+const DataRocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+    setSelectedRocket: (state, action) => {
+        state.data[action.payload].selected = !state.data[action.payload].selected
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getDataRockets.pending, (state) => {
+      state.loading = true
+    }),
+    builder.addCase(getDataRockets.fulfilled, (state, action) => {
+        state.loading = false 
+        const wholedata: any[] = action.payload
+        const outdata: data[] = []
+        wholedata.forEach(element => {
+            outdata.push(
+                {
+                    name: element.name,
+                    image: element.flickr_images[0],
+                    description: element.description,
+                    id: element.id
+                }
+            )
+        })
+        state.data = outdata
+    }),
+    builder.addCase(getDataRockets.rejected, (state, action: PayloadAction<any>) => {
+        state.loading = false,
+        state.error = action.payload
+    })
+  },
+})
+
+export default DataRocketsSlice.reducer
+
+export const { setSelectedRocket } = DataRocketsSlice.actions;
